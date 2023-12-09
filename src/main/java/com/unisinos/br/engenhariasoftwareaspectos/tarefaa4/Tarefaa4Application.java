3af7e5b6a36d36173877a35766544ad5d5857149
Willian Rosa - Koin <willian.rosa@koin.com.br>
2023-12-09 09:46:08 -0300

    initial project

---
+++ src/main/java/com/unisinos/br/engenhariasoftwareaspectos/tarefaa4/Tarefaa4Application.java
@@ -0,0 +1,13 @@
+package com.unisinos.br.engenhariasoftwareaspectos.tarefaa4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tarefaa4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tarefaa4Application.class, args);
+	}
+
+}
